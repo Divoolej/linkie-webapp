97bb9251a3b5a9c333f01291e164c026ba7a2645
Hubert <hubert.dworczynski@gmail.com>
2016-04-24 20:35:23 +0200

    render sidebar in profile route

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  renderTemplate() {
+
+    this.render();
+
+    this.render('sidebar', {
+      outlet: "sidebar",
+      into: "profile"
+    });
+  }
 });
