da90f117cb21f072a52f71102219947d3f7c59a9
Hubert <hubert.dworczynski@gmail.com>
2016-04-24 22:30:46 +0200

    refactor to use ember-sidebars
    https://github.com/ef4/ember-sidebars

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,13 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  renderTemplate() {
-
-    this.render();
-
-    this.render('sidebar', {
-      outlet: "sidebar",
-      into: "profile"
-    });
-  }
 });
