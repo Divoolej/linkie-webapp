b52f4f7d0ef6acdba2cc7e3e7fb2fd094be7b48b
Hubert <hubert.dworczynski@gmail.com>
2016-04-24 15:13:05 +0200

    delete the 'protected' route, move children up a level

--- app/router.js
+++ app/router.js
@@ -6,11 +6,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('protected');
+  this.route('profile');
 
-  this.route('protected', function() {
-    this.route('profile');
+  this.route('profile', function() {
+    this.route('sidebar');
   });
+  
 });
 
 export default Router;
