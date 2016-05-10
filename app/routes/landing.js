ade334aa742b617fe991e80792c575dcee4f8262
Hubert <hubert.dworczynski@gmail.com>
2016-05-10 18:14:54 +0200

    create signInViaFacebook action

--- app/routes/landing.js
+++ app/routes/landing.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    signInViaFacebook: function() {
+      var controller = this.controllerFor('landing');
+      this.get('torii').open('facebook-connect').then(function(authorization) {
+        // TODO send access token to backend
+        debugger;
+      });
+    }
+  }
 });
