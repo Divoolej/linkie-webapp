802836ecd12b32ff4ba2723702bf461497977208
Hubert <hubert.dworczynski@gmail.com>
2016-05-11 22:18:36 +0200

    import isotope dependency, initialize isotope variables

--- app/components/link-list-grid.js
+++ app/components/link-list-grid.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['grid'],
+
+  didInsertElement() {
+    this.$().isotope({
+      // options
+      itemSelector: '.grid-item',
+      layoutMode: 'fitRows'
+    });
+  }
+
 });
