9a259cae87e67d951e508cc2d7b84846722756b3
Hubert <hubert.dworczynski@gmail.com>
2016-05-11 08:43:50 +0200

    redirect to login page when unauthenticated

--- app/torii-adapters/application.js
+++ app/torii-adapters/application.js
@@ -4,7 +4,20 @@ export default Ember.Object.extend({
   open: function(authentication) {
     var accessToken = authentication.accessToken;
     var userId = authentication.userId;
-    console.log('inside torii adapter');
     // TODO send access token to backend
+    return new Ember.RSVP.Promise(function(resolve, reject) {
+      Ember.$.ajax({
+        // TODO fill in backend URL
+        url: 'api/session',
+        data: {'code':accessToken},
+        dataType: 'json',
+        success: Ember.run.bind(null, resolve),
+        error: Ember.run.bind(null, reject)
+      });
+    }).then(function(user){
+      return {
+        currentUser: user
+      }
+    })
   }
 });
