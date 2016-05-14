4bef42295d376f23d9fa9d50523adff1dc086f0b
Hubert <hubert.dworczynski@gmail.com>
2016-05-14 15:44:14 +0200

    rename sidebar category list, remove category list component in favor of inline

--- app/controllers/landing.js
+++ app/controllers/landing.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  storage: Ember.inject.service(),
+
+  willRender() {
+    let token = this.get('storage.token');
+    if (!Ember.isEmpty(token)) {
+      this.get('target').transitionTo('profile');
+    }
+  }
 });
