13e322ca552a59cec7a252c6efcf42683944dc68
Hubert <hubert.dworczynski@gmail.com>
2016-06-02 19:18:33 +0200

    small changes

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return Ember.RSVP.hash({
-      categories: this.store.findAll('category'),
-      links: this.store.findAll('link')
+      categories: this.store.query('category', { userId: 2}),
+      links: this.store.query('link', { userId: 2}),
     });
   }
 });
