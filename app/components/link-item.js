bd308f7bca9cb9ec0beb7a344a4b5b1e85390428
Wojciech Olejnik <divoolej@gmail.com>
2016-06-08 23:22:50 +0200

    Add a destroyLink action in link-item component

--- app/components/link-item.js
+++ app/components/link-item.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  actions: {
+    destroyLink(id) {
+      this.get('store').findRecord('link', id).then(function(link) {
+        link.destroyRecord();
+      });
+    }
+  }
 });
