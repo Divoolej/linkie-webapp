4eadaa414bf7e786fe7979fdfbf20c263bd621ca
Hubert <hubert.dworczynski@gmail.com>
2016-06-08 18:48:51 +0200

    display avatar from facebook

--- app/components/avatar-img.js
+++ app/components/avatar-img.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  storage: Ember.inject.service(),
+
+  url: Ember.computed(function() {
+    return this.get('storage.userAvatarUrl');
+  })
+
 });
