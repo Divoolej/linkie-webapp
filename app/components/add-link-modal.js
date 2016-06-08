97208b89fa5aca9509110edb0c52f274315607a2
Hubert <hubert.dworczynski@gmail.com>
2016-06-08 16:19:09 +0200

    add link submit action

--- app/components/add-link-modal.js
+++ app/components/add-link-modal.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement() {
     Ember.$('select').material_select();
+  },
+
+  actions: {
+    submitLink() {
+      console.log('submitting link');
+    }
   }
 });
