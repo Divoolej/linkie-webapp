ac2cc425d7527b82d532d7c75590d1611eb0637d
Hubert <hubert.dworczynski@gmail.com>
2016-06-08 14:51:06 +0200

    quickfix: don't close sidebar on click

--- app/components/left-sidebar.js
+++ app/components/left-sidebar.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement() {
     this.$(".button-collapse").sideNav({
-      closeOnClick: true
+      closeOnClick: false
     });
     this.$(".search-wrapper").focusin(function() {
       $(this).addClass("focused");
